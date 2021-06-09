30d785eb6c0df94d38453edb28f8c046d4528962
fresh <1326191992@qq.com>
2021-06-09 19:26:01 +0800

    1

--- service-sample2/src/main/java/com/xy/sample2/Sample2Application.java
+++ service-sample2/src/main/java/com/xy/sample2/Sample2Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +12,6 @@ import org.springframework.retry.annotation.EnableRetry;
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan({"com.xy", "com.sc"})
-@RefreshScope
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @EnableFeignClients("com.xy")
